f61d5c951d468d8c8279f27df462a08791736121
Masterpet <eprenza@gmail.com>
2023-08-11 12:06:09 -0400

    se crearon los primeros componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './platzi.webp';
 import './App.css';
+import { TodoCounter } from './componentes/TodoCounter';
+import { TodoSearch } from './componentes/TodoSearch';
+import { TodoList } from './componentes/TodoList';
+import { TodoItem } from './componentes/TodoItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+
+			<TodoCounter />
+			<TodoSearch />
+
+			<TodoList>
+				<TodoItem />
+				<TodoItem />
+				<TodoItem />
+			</TodoList>
+
+			{/* <TodoButton /> */}
+		</div>
+	);
 }
+
 
 export default App;
