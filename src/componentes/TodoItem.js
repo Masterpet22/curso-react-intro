7f6e668089ca070fb1af33e5e030849b63a04344
Masterpet <eprenza@gmail.com>
2023-08-18 10:49:51 -0400

    se agrego la libreria react icons y se le dio estilo a los iconoes

--- src/componentes/TodoItem.js
+++ src/componentes/TodoItem.js
@@ -1,11 +1,12 @@
 import '../style/todoItem.css'
+import { BiCheck, BiX } from "react-icons/bi";
 function TodoItem(props){
 		return(
 			<li className='todoItem'>
 				<span 
-				className={`icon icon-check ${props.completed && 'icon-check-active'}`}onClick={props.onComplete}>✔ {props.completed}</span>
+				className={`icon icon-check ${props.completed && 'icon-check-active'}`}onClick={props.onComplete}> <BiCheck /> {props.completed}</span>
 				<p className={`todoitem-p ${props.completed && 'todoitem-p-completed'}`}>{props.text}</p>
-				<span className={`icon icon-deleted`} onClick={props.onDelete}>❌</span>
+				<span className={`icon icon-deleted`} onClick={props.onDelete}><BiX/></span>
 			</li>
 		)
 }
